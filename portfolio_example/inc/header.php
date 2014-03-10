ea1bfc3a8ad300b5349a27e6bea4b345cb4dc0d3
alex mattingley <alex@capitoldebate.com>
2014-03-10 13:43:59 -0700

    CSS, Classes, ID's, Selecting, positioning
    Went over, fonts, colors, how to use outside fonts, widths and how they interact with parent containers, went over parent containers and how those work, went over floats and went over the clear property.

--- portfolio_example/inc/header.php
+++ portfolio_example/inc/header.php
@@ -1,10 +1,11 @@
 
-<a href="index.php">
-	<h1>Lulight | Love and Light</h1>
+<a href="index.php" id="logo">
+	<h1>Lulight</h1>
+	<h2>Love and Light</h2>
 </a>
 <nav>
 	<ul>
-		<li><a href="index.php">Portfolio</a></li>
+		<li><a href="index.php" class="selected">Portfolio</a></li>
 		<li><a href="about.php">About</a></li>
 		<li><a href="contact.php">Contact Me</a></li>
 	</ul>
--- portfolio_example/index.php
+++ portfolio_example/index.php
@@ -4,6 +4,8 @@
 	<meta charset="utf-8">
 	<title>Lulight | Love and Light</title>
 	<link rel="stylesheet" href="css/normalize.css">
+	<link href='http://fonts.googleapis.com/css?family=Changa+One|Open+Sans:400,700,700italic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
@@ -12,46 +14,48 @@
 		include('inc/header.php');
 		?>
 	</header>
-	<section>
-		<ul>
-			<li>
-				<a href="img/numbers-01.jpg">
-					<img src="img/numbers-01.jpg" alt="">
-					<p>Landscapes/Architecture</p>
-				</a>
-			</li>
-			<li>
-				<a href="img/numbers-02.jpg">
-					<img src="img/numbers-02.jpg" alt="">
-					<p>Fantasy Photography</p>
-				</a>
-			</li>
-			<li>
-				<a href="img/numbers-06.jpg">
-					<img src="img/numbers-06.jpg" alt="">
-					<p>Weddings</p>
-				</a>
-			</li>
-			<li>
-				<a href="img/numbers-09.jpg">
-					<img src="img/numbers-09.jpg" alt="">
-					<p>Families</p>
-				</a>
-			</li>
-			<li>
-				<a href="img/numbers-12.jpg">
-					<img src="img/numbers-12.jpg" alt="">
-					<p>Experimentation with color</p>
-				</a>
-			</li>
-		</ul>
-		
-	</section>
-	<footer>
-		<?php 
-		include('inc/footer.php');
-		?>
-	</footer>
+	<div id="wrapper">
+		<section>
+			<ul id="gallery">
+				<li>
+					<a href="img/numbers-01.jpg">
+						<img src="img/numbers-01.jpg" alt="">
+						<p>Landscapes and Architecture</p>
+					</a>
+				</li>
+				<li>
+					<a href="img/numbers-02.jpg">
+						<img src="img/numbers-02.jpg" alt="">
+						<p>Fantasy and meditative Photography</p>
+					</a>
+				</li>
+				<li>
+					<a href="img/numbers-06.jpg">
+						<img id="clear" src="img/numbers-06.jpg" alt="">
+						<p>Weddings</p>
+					</a>
+				</li>
+				<li>
+					<a href="img/numbers-09.jpg">
+						<img src="img/numbers-09.jpg" alt="">
+						<p>Families</p>
+					</a>
+				</li>
+				<li>
+					<a href="img/numbers-12.jpg">
+						<img src="img/numbers-12.jpg" alt="">
+						<p>Experimentation with colors</p>
+					</a>
+				</li>
+			</ul>
+			
+		</section>
+		<footer>
+			<?php 
+			include('inc/footer.php');
+			?>
+		</footer>
+	</div>	
 
 </body>
 </html>
