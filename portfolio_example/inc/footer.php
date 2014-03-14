30fc9ff0ef392fe08ee21cedc514830c9c245da8
alex mattingley <alex@capitoldebate.com>
2014-03-14 11:10:44 -0700

    Shortcuts document, more CSS
    Media Queries (break points) responsive design, more information on selectors.

--- portfolio_example/inc/footer.php
+++ portfolio_example/inc/footer.php
@@ -1,14 +1,12 @@
-<!--<section> opening tag is in header.php-->
-</section>
-<footer>
-	<a href="https://twitter.com/_FloridaMan">
-		<img src="img/twitter-wrap.png" alt="Twitter" class="social-icon">
-	</a>
-	<a href="https://www.facebook.com/lulightphotography">
-		<img src="img/facebook-wrap.png" alt="facebook" class="social-icon">
-	</a>
-	<p>&copy; 2014 Lucia Ferreira</p>
-</footer>
-</div>	
-</body>
+			<footer>
+				<a href="https://twitter.com/_FloridaMan">
+					<img src="img/twitter-wrap.png" alt="Twitter" class="social-icon">
+				</a>
+				<a href="https://www.facebook.com/lulightphotography">
+					<img src="img/facebook-wrap.png" alt="facebook" class="social-icon">
+				</a>
+				<p>&copy; 2014 Lucia Ferreira</p>
+			</footer>
+		</div>	
+	</body>
 </html>
--- portfolio_example/inc/header.php
+++ portfolio_example/inc/header.php
@@ -8,21 +8,23 @@
 	<link rel="stylesheet" href="css/normalize.css">
 	<link href='http://fonts.googleapis.com/css?family=Changa+One|Open+Sans:400,700,700italic' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/responsive.css">
+
 </head>
 <body>
-<header>
-<a href="index.php" id="logo">
-	<h1>Lulight</h1>
-	<h2>Love and Light</h2>
-</a>
-<nav>
-	<ul>
-		<li><a href="index.php" class="<?php if ($section == "portfolio") { echo "selected"; } ?>">Portfolio</a></li>
-		<li><a href="about.php" class="<?php if ($section == "about") { echo "selected"; } ?>">About</a></li>
-		<li><a href="contact.php" class="<?php if ($section == "contact") { echo "selected"; } ?>">Contact Me</a></li>
-	</ul>
-</nav>
-</header>
-<div id="wrapper">
-		<section>
-<!--</section> close is in footer.php-->
+	<header>
+	<a href="index.php" id="logo">
+		<h1>Lulight</h1>
+		<h2>Love and Light</h2>
+	</a>
+	<nav>
+		<ul>
+			<li><a href="index.php" class="<?php if ($section == "portfolio") { echo "selected"; } ?>">Portfolio</a></li>
+			<li><a href="about.php" class="<?php if ($section == "about") { echo "selected"; } ?>">About</a></li>
+			<li><a href="contact.php" class="<?php if ($section == "contact") { echo "selected"; } ?>">Contact Me</a></li>
+		</ul>
+	</nav>
+	</header>
+	<div id="wrapper">
+			<section>
+	<!--</section> close is in footer.php-->
