765ab080e8a8328e7abe21eba88545c2779faa02
alex mattingley <alex@capitoldebate.com>
2014-03-17 15:51:00 -0700

    Methods in objects

--- OOP_deep_dive/index.php
+++ OOP_deep_dive/index.php
@@ -11,3 +11,26 @@ $p->name = "Space juice"; //defining the property.
 echo $p->name; //pay close attention to the lack of $ signs.
  
 ?>
+
+
+<!-- METHODS -->
+<?php
+class Product {
+
+	//property
+	public $name = 'default_name';
+
+	//method
+	public function getInfo(){
+		return "Product Name: " . $this->name;
+	}
+}
+
+$p = new Product();
+
+echo $p->getInfo();
+
+?>
+
+<!-- NOTE: BOTH OF THESE CHUNKS OF CODE WILL NOT WORK AT THE SAME TIME, 
+YOU NEED TO COMMENT ONE OUT AT A TIME.S -->
