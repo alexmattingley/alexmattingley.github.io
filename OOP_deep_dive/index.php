0f41118cfdc734251195171609cef22983cd1583
alex mattingley <alex@capitoldebate.com>
2014-03-24 09:58:29 -0700

    Static properties and methods

--- OOP_deep_dive/index.php
+++ OOP_deep_dive/index.php
@@ -16,7 +16,8 @@
 <?php
 class Product {
 
-	//property
+	public static $manufacturer = "bart taylor";
+	//properties
 	public $name = 'default_name';
 	public $price = 0;
 	public $desc = 'default description';
@@ -31,25 +32,34 @@ class Product {
 	public function getInfo(){
 		return "Product Name: " . $this->name;
 	}
-}
- class soda extends Product {
- 	
- 	public $flavor;
 
- 	function __construct($name, $price, $desc, $flavor){ //This is adding functionality to the parrent class without reimplementing completely
-		parent::__construct($name, $price, $desc);
-		$this->flavor = $flavor;
+	public function get_maker(){
+		return self::$manufacturer . "<br>";
 	}
+}
+ // class soda extends Product {
+ 	
+ // 	public $flavor;
 
- 	public function getInfo(){
- 		return "Product Name: " . $this->name . " Flavor :" . $this->flavor; //this inherts some properties from class product (i.e $name)
- 	}
- }
+ // 	function __construct($name, $price, $desc, $flavor){ //This is adding functionality to the parrent class without reimplementing completely
+	// 	parent::__construct($name, $price, $desc);
+	// 	$this->flavor = $flavor;
+	// }
+
+ // 	public function getInfo(){
+ // 		return "Product Name: " . $this->name . " Flavor :" . $this->flavor; //this inherts some properties from class product (i.e $name)
+ // 	}
+ // }
 
 $shirt = new Product("Space Juice T-shirt", 20, "Awesome Grey T-shirt");
-$soda = new soda("Space Juice Soda", 2, "Thirst Mutilator", "grape");
-echo $soda->getInfo();
+//$soda = new soda("Space Juice Soda", 2, "Thirst Mutilator", "grape");
+// echo $soda->getInfo();
 echo $shirt->getInfo();
+?>
+<br>
+<?php
+echo $shirt->get_maker(); //One way to access the static method get_maker();
+echo $shirt::$manufacturer;
 
 ?>
 
