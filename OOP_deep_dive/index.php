fd151fdb48a372a2ba699c4d363f6a32a8a1be44
alex mattingley <alex@capitoldebate.com>
2014-03-24 08:57:23 -0700

    Information and Notes of CSS but more importantly, another step toward objects.

--- OOP_deep_dive/index.php
+++ OOP_deep_dive/index.php
@@ -1,14 +1,13 @@
-<?php
-class Product 
-{
- 	public $name; //scope of the property
-} 
+ <?php
+// class Products {
+//  	public $names; //scope of the property
+// } 
 
  
-$p = new Product(); 
+// $p = new Products(); 
 
-$p->name = "Space juice"; //defining the property.
-echo $p->name; //pay close attention to the lack of $ signs.
+// $p->names = "Space juice"; //defining the property.
+// echo $p->names; //pay close attention to the lack of $ signs.
  
 ?>
 
@@ -19,18 +18,40 @@ class Product {
 
 	//property
 	public $name = 'default_name';
+	public $price = 0;
+	public $desc = 'default description';
+
+	function __construct($name, $price, $desc){
+		$this->name = $name;
+		$this->price = $price;
+		$this->desc = $desc;
+	}
 
 	//method
 	public function getInfo(){
 		return "Product Name: " . $this->name;
 	}
 }
+ class soda extends Product {
+ 	
+ 	public $flavor;
 
-$p = new Product();
+ 	function __construct($name, $price, $desc, $flavor){ //This is adding functionality to the parrent class without reimplementing completely
+		parent::__construct($name, $price, $desc);
+		$this->flavor = $flavor;
+	}
 
-echo $p->getInfo();
+ 	public function getInfo(){
+ 		return "Product Name: " . $this->name . " Flavor :" . $this->flavor; //this inherts some properties from class product (i.e $name)
+ 	}
+ }
+
+$shirt = new Product("Space Juice T-shirt", 20, "Awesome Grey T-shirt");
+$soda = new soda("Space Juice Soda", 2, "Thirst Mutilator", "grape");
+echo $soda->getInfo();
+echo $shirt->getInfo();
 
 ?>
 
-<!-- NOTE: BOTH OF THESE CHUNKS OF CODE WILL NOT WORK AT THE SAME TIME, 
-YOU NEED TO COMMENT ONE OUT AT A TIME.S -->
+
+<!-- NOTE: You cannot have two classes with the same name. -->
