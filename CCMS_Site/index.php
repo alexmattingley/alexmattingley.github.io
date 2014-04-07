1b1250469623a335675d5e6fb3faa6710825d1a4
alex mattingley <alex@capitoldebate.com>
2014-04-07 14:43:33 -0700

    Everything but the footer
    Everything but the footer is basically laid out and contains most styling. Still needs some work though

--- CCMS_Site/index.php
+++ CCMS_Site/index.php
@@ -63,7 +63,7 @@
 			<h2>Center for Computational Mass Spectrometry</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis ultrices orci, ut volutpat urna porttitor in.</p>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis ultrices orci, ut volutpat urna porttitor in. Um sagittis ultrices orci, ut volutpat urna porttitor in.</p>
-			<a href="#"></a><button id="button_green_bg">About Us</button>
+			<a href="#"><button id="button_green_bg">About Us</button></a>
 		</div>
 	</section>
 	<section class="secondary_bg_color">
@@ -72,14 +72,57 @@
 			<h2>Serving the Biomedical Research Community</h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis ultrices orci, ut volutpat urna porttitor in.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis ultrices orci, ut volutpat urna porttitor in.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis ultrices orci, ut volutpat urna porttitor in.</p>
 			<ul class="button_display">
-					<li><a href="#"></a><button id="button_green_bg">Get Sarted</button></li>
-					<li><a href="#"></a><button id="button_green_txt">Learn More</button></li>
+					<li><a href="#"><button id="button_green_bg">Get Sarted</button></a></li>
+					<li><a href="#"><button id="button_green_txt">Learn More</button></a></li>
 			</ul>
 		</div>
 		<div class="section_desc section_desc_img">
 			<h4>University of California San Diego</h4>
 			<h3>Department of Computer Science & Engineering</h3>
 		</div>
+	</section>
+	<section>
+		<div class="section_desc">
+			<h4>Results that Matter</h4>
+			<h2>See What CCMS Can Do</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis ultrices orci, ut volutpat urna porttitor in. Um sagittis</p>
+		</div>
+		<ul class="section_feed">
+			<li>
+				<div class="secondary_bg_color">
+					<img src="http://lorempixel.com/320/200">
+					<h4>Latest Projects</h4>
+					<p class="truncate">Peptide fragmentation models for new enzymes</p>
+					<h6>Collaborator: Elizabeth Komives</h6>
+				</div>	
+				<a class="button" href="#">View All</a>
+			</li>
+			<li>
+				<div class="secondary_bg_color">
+					<img src="http://lorempixel.com/320/200">
+					<h4>Software Tools</h4>
+					<p>SpliceDB: Splice Graph Proteomics Tools</p>
+					<h6>PI Contact: Vineet Bafna </h6>
+				</div>
+				<a class="button" href="#">View All</a>
+			</li>
+			<li>
+				<div class="secondary_bg_color">
+					<img src="http://lorempixel.com/320/200">
+					<h4>CCMS Training</h4>
+					<p>RECOMB Satellite Conference on Computational Proteomics.</p>
+					<h6>Location: UCSD Calit2 Building</h6>
+				</div>
+				<a class="button" href="#">View All</a>
+			</li>
+		</ul>
+	</section>
+	<section class="primary_color_bg">
+		<div class="section_desc">
+			<h2>Make Sense of your Research Data</h2>
+			<h5>Contact us and start gaining insight into your research.</h5>
+			<a class="button white" href="#">Get Started</a>
+		</div>	
 	</section>	
 </body>
 </html>
