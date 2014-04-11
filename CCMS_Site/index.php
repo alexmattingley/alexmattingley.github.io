0b5bbacab376099b455471dfb36988c1195341cb
alex mattingley <alex@capitoldebate.com>
2014-04-11 10:03:22 -0700

    Tablet Size Footer Finished

--- CCMS_Site/index.php
+++ CCMS_Site/index.php
@@ -156,29 +156,31 @@
 			</li>
 		</ul>
 	</section>
-	<footer>
+	<footer class="group">
 		<a class="up-arrow" href="#">
 			<i class="fa fa-arrow-up fa-lg"></i>
 		</a>
-		<ul class="footer main-nav">
-			<li><a href="#">About</a></li>
-			<li><a href="#">Research Areas</a></li>
-			<li><a href="#">Projects</a></li>
-			<li><a href="#">Software Tools</a></li>
-			<li><a href="#">Service</a></li>
-			<li><a href="#">Resources</a></li>
-			<li><a href="#">Sign In</a></li>
-		</ul>
-		<a href="#" class="footer-logo"></a>
-		<ul class="social_media_foot">
-			<li><a href="#"><i class="fa fa-facebook-square fa-2x"></i></a></li>
-			<li><a href="#"><i class="fa fa-twitter-square fa-2x"></i></a></li>
-			<li><a href="#"><i class="fa fa-linkedin-square fa-2x"></i></a></li>
-			<li><a href="#"><i class="fa fa-google-plus-square fa-2x"></i></a></li>
-		</ul>
-		<div class="copyright">
-			<h6>Copyright &copy; <?php echo date("Y") ?> The regents of the University of California</h6>
-			<h6>All Rights Reserved</h6>
+		<div class="inner_footer">
+			<ul class="footer main-nav">
+				<li><a href="#">About</a></li>
+				<li><a href="#">Research Areas</a></li>
+				<li><a href="#">Projects</a></li>
+				<li><a href="#">Software Tools</a></li>
+				<li><a href="#">Service</a></li>
+				<li><a href="#">Resources</a></li>
+				<li><a href="#">Sign In</a></li>
+			</ul>
+			<a href="#" class="footer-logo"></a>
+			<ul class="social_media_foot">
+				<li><a href="#"><i class="fa fa-facebook-square fa-2x"></i></a></li>
+				<li><a href="#"><i class="fa fa-twitter-square fa-2x"></i></a></li>
+				<li><a href="#"><i class="fa fa-linkedin-square fa-2x"></i></a></li>
+				<li><a href="#"><i class="fa fa-google-plus-square fa-2x"></i></a></li>
+			</ul>
+			<div class="copyright">
+				<h6>Copyright &copy; <?php echo date("Y") ?> The regents of the University of California</h6>
+				<h6>All Rights Reserved</h6>
+			</div>
 		</div>
 	</footer>	
 </body>
