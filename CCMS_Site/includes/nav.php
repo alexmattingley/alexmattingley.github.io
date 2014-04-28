84581aaa7cd232e30012ae450040bb9e641d9ec8
alex mattingley <alex@capitoldebate.com>
2014-04-28 11:42:40 -0700

    Having a working drop down nav in nav.php, going to experiment with z-index

--- CCMS_Site/includes/nav.php
+++ CCMS_Site/includes/nav.php
@@ -1,6 +1,22 @@
 <ul class="main-nav nav-collapse js">
-	<li><a href="#">About</a></li>
-	<li><a href="#">Research Areas</a></li>
+	<li>
+		<a href="#">About</a>
+		<ul class="dropdown_nav">
+			<li><a href="#">CCMS Team</a></li>
+		</ul>
+	</li>
+	<li>
+		<a href="#">Research Areas</a>
+		<ul class="dropdown_nav">
+			<li><a href="#">Proteogenomics</a></li>
+			<li><a href="#">antibiotic Sequenciing</a></li>
+			<li><a href="#">Spectral Networks</a></li>
+			<li><a href="#">Universal Tools for Mass Spectrometry</a></li>
+			<li><a href="#">Universal Tools for Mass Spectrometry</a></li>
+			<li><a href="#">Universal Tools for Mass Spectrometry</a></li>
+			<li><a href="#">Universal Tools for Mass Spectrometry</a></li>
+		</ul>
+	</li>
 	<li><a href="#">Projects</a></li>
 	<li><a href="#">Software Tools</a></li>
 	<li><a href="#">Service</a></li>
